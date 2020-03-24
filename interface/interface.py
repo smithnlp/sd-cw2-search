b149fc08c9616cab6665f1e177c5c93b500772a8
smithnlp <smithnlp@users.noreply.github.com>
2020-03-24 00:05:51 +0000

    remove pandas dependency and refactor loading csv into ES

--- interface/interface.py
+++ interface/interface.py
@@ -4,12 +4,12 @@
 3. Load data from file into ES
 4. Start the searchbar
 """
+import csv
 import json
 import requests
 import sys
 
 import click
-import pandas as pd
 from prompt_toolkit.shortcuts import prompt
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
@@ -24,7 +24,7 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 class CustomCompleter(Completer):
     def get_completions(self, document, complete_event):
         word = document.get_word_before_cursor()
-        result = es.search(index="games_df", body={"size" : 50, "query": {"prefix" : { "name" : word }}})
+        result = es.search(index="games", body={"size" : 50, "query": {"prefix" : { "name" : word }}})
         if result:
             # could make this conditional on some score threshold (if hits['_score'] > thresh)
             top = [hit['_source']['name'] for hit in result['hits']['hits']]
@@ -35,24 +35,13 @@ class CustomCompleter(Completer):
             yield Completion('completion', start_position=-len(word))
 
 
-def rec_to_actions(df):
-    """
-    https://stackoverflow.com/questions/49726229/how-to-export-pandas-data-to-elasticsearch
-    Helper for bulk loading
-    """
-    for record in df.to_dict(orient="records"):
-        yield ('{ "index" : { "_index" : "games_df", "_type" : "game" }}')
-        yield (json.dumps(record))
-
-
 def load_data():
     """
     """
-    df = pd.read_csv('games.csv')
-    r = es.bulk(rec_to_actions(df))
-    if r["errors"]:
-        print(f'\nError loading data into elasticsearch.\n')
-        sys.exit(1)
+    with open('games.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for i, row in enumerate(reader):
+            es.index(index='games', doc_type='game', id=i, body=json.dumps(dict(row)))
 
 
 def min_2_hr(mins):
@@ -100,7 +89,7 @@ def searchbar():
             print(f'\nQuitting. Thanks!\n\n{zero_results_count}/{search_count} searches returned zero results.\n')
         else:
             search_count += 1
-            result = es.search(index="games_df", body={"size" : 10, "query": {"multi_match" : { "query" : user_input, "fields": ["name^2", "designer"]}}})
+            result = es.search(index="games", body={"size" : 10, "query": {"multi_match" : { "query" : user_input, "fields": ["name^2", "designer"]}}})
             if result:
                 hits = [hit['_source'] for hit in result['hits']['hits']]
                 num_results = f'\nDisplaying {len(hits)}/{len(hits)} results\n'
@@ -111,13 +100,13 @@ def searchbar():
                     results += f'\n\tCreated by {hit["designer"]}\n'
 
                     rated, players, pt, min, comp, votes = 'Rated:', 'Players:', 'Playing time:', 'Rec. minimum age:', 'Complexity:', 'Number of votes:'
-                    results += f'\n\t|{rated:>20}\t {hit["avg_rating"]:.1f}/10.0'
+                    results += f'\n\t|{rated:>20}\t {float(hit["avg_rating"]):.1f}/10.0'
                     results += f'\n\t|{players:>20}\t {hit["min_players"]}-{hit["max_players"]}'
-                    results += f'\n\t|{pt:>20}\t~{min_2_hr(hit["playing_time"])}'
+                    results += f'\n\t|{pt:>20}\t~{min_2_hr(int(hit["playing_time"]))}'
                     results += f'\n\t|'
-                    results += f'\n\t|{comp:>20}\t {comp_level(hit["complexity_out_of_5"])} ({hit["complexity_out_of_5"]:.1f}/5.0)'
+                    results += f'\n\t|{comp:>20}\t {comp_level(float(hit["complexity_out_of_5"]))} ({float(hit["complexity_out_of_5"]):.1f}/5.0)'
                     results += f'\n\t|{min:>20}\t {hit["recommended_min_age"]} years old'
-                    results += f'\n\t|{votes:>20}\t {format(hit["num_votes"], ",")}\n'
+                    results += f'\n\t|{votes:>20}\t {format(int(hit["num_votes"]), ",")}\n'
 
                     results += f'\n\tCategory: {hit["category"]}'
                     results += f'\n\tGame-play: {hit["mechanic"]}\n\n'
