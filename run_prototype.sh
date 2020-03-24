748e08f9ec9996b2a0542f7e4fa80c1637bb760e
smithnlp <smithnlp@users.noreply.github.com>
2020-03-24 13:59:13 +0000

    echo updates during process

--- run_prototype.sh
+++ run_prototype.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
+echo
+echo "Checking for latest updates"
+echo
+
 # make sure you have the latest version
 git pull &&
+
+echo
+echo "Building images"
+echo
 
 # build the images, and re-build them if you've run this before for clean slate
 # https://docs.docker.com/compose/reference/build/
 docker-compose build --no-cache &&
 
-# spin up the containers using those images and open the interface service interactively
+echo
+echo "Build successful!"
+echo
+echo "Starting prototype. (Elasticsearch will take a brief moment to load)"
+echo
+
+# spin up both containers using those images and open the "interface" service interactively
 # https://docs.docker.com/compose/reference/run/
 docker-compose run interface
 
-# clean up
+echo
+echo "End of prototype. Tidying up."
+echo
+
+# tidy up
 # https://docs.docker.com/compose/reference/down/
 docker-compose down &&
-docker image rm $(docker image ls --filter="label=sdproto=true" --quiet)
+docker image rm $(docker image ls --filter="label=sdproto=true" --quiet) &&
+
+echo
+echo "Tidying up complete!"
+echo
