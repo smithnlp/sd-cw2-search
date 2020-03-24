eb1e9a3ec88ff4f72b28d7eadcef29c1eadf190e
smithnlp <smithnlp@users.noreply.github.com>
2020-03-24 14:25:14 +0000

    add -- for clean output

--- run_prototype.sh
+++ run_prototype.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 echo
+echo "--------------------------------------------------------------------------------"
 echo "Checking for latest updates"
+echo "--------------------------------------------------------------------------------"
 echo
 
 # make sure you have the latest version
 git pull &&
 
 echo
+echo "--------------------------------------------------------------------------------"
 echo "Building images"
+echo "--------------------------------------------------------------------------------"
 echo
 
 # build the images, and re-build them if you've run this before for clean slate
@@ -16,9 +20,13 @@ echo
 docker-compose build --no-cache &&
 
 echo
+echo "--------------------------------------------------------------------------------"
 echo "Build successful!"
+echo "--------------------------------------------------------------------------------"
 echo
+echo "--------------------------------------------------------------------------------"
 echo "Starting prototype. (Elasticsearch will take a brief moment to load)"
+echo "--------------------------------------------------------------------------------"
 echo
 
 # spin up both containers using those images and open the "interface" service interactively
@@ -26,7 +34,9 @@ echo
 docker-compose run interface
 
 echo
+echo "--------------------------------------------------------------------------------"
 echo "End of prototype. Tidying up."
+echo "--------------------------------------------------------------------------------"
 echo
 
 # tidy up
@@ -35,5 +45,7 @@ docker-compose down &&
 docker image rm $(docker image ls --filter="label=sdproto=true" --quiet) &&
 
 echo
+echo "--------------------------------------------------------------------------------"
 echo "Tidying up complete!"
+echo "--------------------------------------------------------------------------------"
 echo
